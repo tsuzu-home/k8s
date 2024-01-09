175175c6c4e13d434bcf0c2d4cb1d22566f628bc
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2024-01-10 02:50:38 +0900

    Add controller to configure ConfigMap for kube-vip-cloud-provider

---
+++ manifests/infra-cluster/kube-vip-cloud-provider/kubevip-config-controller/controller.mjs
@@ -0,0 +1,38 @@
+import ip from 'ip';
+
+const reconcile = async () => {
+    const addrInfo = JSON.parse((await $`ip --json a show dev br0`).stdout)[0].addr_info
+    console.log(addrInfo)
+    const v6CIDR = addrInfo.filter((addr) => addr.family === "inet6" && addr.scope === "global")[0]
+    console.log(v6CIDR)
+
+    const buf = ip.toBuffer(v6CIDR.local);
+    console.log(buf)
+    buf[56 / 8] = 0xff;
+    for (let i = 64 / 8; i < 128 / 8; i++) {
+        buf[i] = 0x00;
+    }
+
+    const globalCIDR = ip.toString(buf) + "/64";
+    console.log(globalCIDR);
+
+    const patcher = $`kubectl patch configmap/kubevip -n kube-system --type merge --patch-file /dev/stdin`;
+
+    patcher.stdin.write(JSON.stringify({
+        data: {
+            "cidr-global": globalCIDR,
+        },
+    }));
+    patcher.stdin.end();
+
+    await patcher;
+}
+
+const processor = () => {
+    reconcile().catch((err) => {
+        console.error(err);
+    });
+}
+
+processor();
+setInterval(processor, 60 * 1000);
