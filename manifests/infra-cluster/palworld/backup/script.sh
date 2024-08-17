84289e2ea7b547c380fea598727fc9b272d0841f
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2024-08-18 00:14:44 +0900

    restic forget for PalWorld backup

--- manifests/infra-cluster/palworld/backup/script.sh
+++ manifests/infra-cluster/palworld/backup/script.sh
@@ -6,3 +6,4 @@ source /root/.aws/.env
 
 restic -r s3:http://10.24.0.60:31571/palworld init || true
 restic -r s3:http://10.24.0.60:31571/palworld --verbose backup /mnt/source/steamapps/common/PalServer/Pal/Saved/SaveGames
+restic -r s3:http://10.24.0.60:31571/palworld forget --group-by=paths --keep-monthly 6 --keep-daily 7 --keep-last 36 --prune --repack-uncompressed
